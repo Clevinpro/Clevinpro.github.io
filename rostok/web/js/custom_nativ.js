d5e5ecb7958db476dac7216d68f9f2f79b0b2b9f
clevin <clevin.pro@gmail.com>
2016-09-01 12:53:15 +0300

    reDesign

--- rostok/web/js/custom_nativ.js
+++ rostok/web/js/custom_nativ.js
@@ -1,14 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    var navbar = document.querySelector('.navbar-fixed-top');
-	(function () {
-		if (window.pageYOffset > 0) {
-			navbar.setAttribute("data-spy", "affix");
-		}
-	})();
-	
-	
-
 	window.onscroll = onChangePos;
     var logo = document.querySelector('.navbar-brand img');
 	function onChangePos() {
