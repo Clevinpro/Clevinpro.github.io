70013d6e6a03b328b6a1bf236b05d7559693dba0
clevin <clevin.pro@gmail.com>
2016-09-01 09:51:18 +0300

    reDesign

--- rostok/web/js/custom_nativ.js
+++ rostok/web/js/custom_nativ.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	function onChangePos() {
     var header = document.getElementById("navbar");
-    var logo = document.querySelector('.navbar-brand img');
+    var logo = document.querySelector('.affix .navbar-brand img');
     if (window.pageYOffset > 0) {
 		logo.setAttribute("src", "web/img/logo-b.png");
     } else {
