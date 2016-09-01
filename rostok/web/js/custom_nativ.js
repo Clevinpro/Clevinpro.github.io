72d2ab7456a8571c7b2c78fd52b30cd67c08cd06
clevin <clevin.pro@gmail.com>
2016-09-01 14:44:33 +0300

    reDesign

--- rostok/web/js/custom_nativ.js
+++ rostok/web/js/custom_nativ.js
@@ -2,11 +2,20 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	window.onscroll = onChangePos;
     var logo = document.querySelector('.navbar-brand img');
+    var nav = document.querySelector('nav');
 	function onChangePos() {
     if (window.pageYOffset > 0) {
 		logo.setAttribute("src", "web/img/logo-b.png");
+		document.querySelector('.navbar-nav li:nth-of-type(1) img').setAttribute('src', 'web/img/binst.png');
+		document.querySelector('.navbar-nav li:nth-of-type(2) img').setAttribute('src', 'web/img/bvk.png');
+		document.querySelector('.navbar-nav li:nth-of-type(3) img').setAttribute('src', 'web/img/bfb.png');
+		nav.classList.add("scroll");
     }else {
         logo.setAttribute("src", "web/img/logo.png");
+        document.querySelector('.navbar-nav li:nth-of-type(1) img').setAttribute('src', 'web/img/inst.png');
+		document.querySelector('.navbar-nav li:nth-of-type(2) img').setAttribute('src', 'web/img/vk.png');
+		document.querySelector('.navbar-nav li:nth-of-type(3) img').setAttribute('src', 'web/img/fb.png');
+		nav.classList.remove("scroll");
     }
 }
 
